547405e92aca2f20e91c3f6e7b36651f71fade9d
suffering <zhuboliu@gmail.com>
2014-08-08 14:19:21 +0800

    put functions to closure

--- js/app.js
+++ js/app.js
@@ -1,86 +1,3 @@
-var string2literal = function (value){
-  var maps = {
-   "NaN": NaN,
-   "null": null,
-   "undefined": undefined,
-   "0": 0,
-   "1": 1,
-   "2": 2,
-   "true": true,
-   "false": false,
-   "Infinity": Infinity,
-   "-Infinity": -Infinity
-   }
-  return ((value in maps) ? maps[value] : value);
-};
-
-function binary(special_values, operators, mode){
-    var counter   = 0;
-  var box = []
-    for(var i=0;i < operators.length;i++){
-        for(var k=0; k < special_values.length;k++){
-      var nvalues = mode ? special_values.slice(k) : special_values;
-            for(var j=0; j < nvalues.length;j++){
-                //n^3, consider the problem that `a + b != b + a`
-                try{
-                    counter++; //the prefix
-          var tmp = []
-                    var vk = string2literal(special_values[k]), vj = string2literal(nvalues[j]), oi = operators[i];
-                    // if(typeof vk == "string")
-//                         vk = "\'" + vk + "\'";
-//                     if(typeof vj == "string")
-//                         vj = "\'" + vj + "\'";
-                    var string_to_eval = vk + ' ' + oi + ' ' + vj;
-                    //if not wrap string with single quto, run in eval function will raise a referenceError, the eval will treat the string as a variable;
-                    // var string_to_to_eval = [vk,oi,vj].join(' ');
-                    // the previous line will throw error in some cases;
-          box.push([special_values[k], oi, nvalues[j],("" + string_to_eval + ""), eval(string_to_eval)])
-                    console.log(counter + ": " + string_to_eval + ": ");//prefix hint
-                    console.log(eval(string_to_eval)); //run it
-                    console.log('');//generate a new line.
-                }catch(e){
-                    console.log(e);
-                    continue;
-                }
-            }// end the enum, special_values, deep 2
-        }// end the enum, special_values, deep 1
-    }// end the enum, arithmatic_operators
-  console.log(box)
-  return box;
-}//
-
-function unary(special_values, unary_operators){
-    var counter = 0;
-  var box = [];
-    for(var i=0; i < unary_operators.length; i++){
-        for(var k=0; k < special_values.length; k++){
-            try{
-                counter++;
-                var vk = string2literal(special_values[k]), oi = string2literal(unary_operators[i]), seperator = '';
-                //set the default seperator to empty string is just for reading habit.
-                // if(typeof vk == "string")
-//                     vk = "\'" + vk + "\'";
-                if(oi.length > 2)//if the length of the operator greater than 2, for example `void`, `typeof`, there should be at least one space between the operator and operand.
-                    seperator = ' ';
-                var string_to_eval = oi + seperator + vk; 
-        box.push([oi, special_values[k], ("" + string_to_eval + ""), eval(string_to_eval)])
-                console.log(counter + ": " + string_to_eval + ": ");//prefix hint
-                console.log(eval(string_to_eval)); //run it
-                console.log('');//generate a new line.
-            }catch(e){
-                console.log(e);
-                continue;
-            }
-        }// end of enum, special_values
-    }// end of enum, unary_operators
-  console.log(box)
-  return box
-}//end of unary function
-
-var app = angular.module('app', ['checklist-model']);
-
-// no keyword $scope, just a function, implicit declearing, use the keyword `this`
-
 Quirks = function(){
   var special_values       = [NaN, null, undefined, 0, 1, 2, true, false, Infinity, -Infinity, " ", "a"];
   var arithmatic_operators = ["+", "-", "*", "/", '%'];
@@ -95,9 +12,81 @@ Quirks = function(){
     logical: logical_operators,
     equality: equality_operators
   }
-  return {values: special_values, operators: operators, unary: unary_operators};
+  var string2literal = function (value){
+    var maps = {
+     "NaN": NaN,
+     "null": null,
+     "undefined": undefined,
+     "0": 0,
+     "1": 1,
+     "2": 2,
+     "true": true,
+     "false": false,
+     "Infinity": Infinity,
+     "-Infinity": -Infinity
+     }
+    return ((value in maps) ? maps[value] : value);
+  };
+
+  function run_binary(special_values, operators, mode){
+      var counter   = 0;
+      var box = [];
+      for(var i=0;i < operators.length;i++){
+          for(var k=0; k < special_values.length;k++){
+        var nvalues = mode ? special_values.slice(k) : special_values;
+              for(var j=0; j < nvalues.length;j++){
+                  //n^3, consider the problem that `a + b != b + a`
+                  try{
+                      counter++; //the prefix
+                      var tmp = []
+                      var vk = string2literal(special_values[k]), vj = string2literal(nvalues[j]), oi = operators[i];
+                      var string_to_eval = vk + ' ' + oi + ' ' + vj;
+                      //if not wrap string with single quto, run in eval function will raise a referenceError, the eval will treat the string as a variable;
+                      box.push([special_values[k], oi, nvalues[j],("" + string_to_eval + ""), eval(string_to_eval)])
+                      console.log(counter + ": " + string_to_eval + ": ");//prefix hint
+                      console.log(eval(string_to_eval)); //run it
+                      console.log('');//generate a new line.
+                  }catch(e){
+                      console.log(e);
+                      continue;
+                  }
+              }// end the enum, special_values, deep 2
+          }// end the enum, special_values, deep 1
+      }// end the enum, arithmatic_operators
+    console.log(box)
+    return box;
+  }//the end of run_binary
+
+  function run_unary(special_values, unary_operators){
+      var counter = 0;
+      var box = [];
+      for(var i=0; i < unary_operators.length; i++){
+          for(var k=0; k < special_values.length; k++){
+              try{
+                  counter++;
+                  var vk = string2literal(special_values[k]), oi = string2literal(unary_operators[i]), seperator = '';
+                  //set the default seperator to empty string is just for reading habit.
+                  if(oi.length > 2)//if the length of the operator greater than 2, for example `void`, `typeof`, there should be at least one space between the operator and operand.
+                      seperator = ' ';
+                  var string_to_eval = oi + seperator + vk; 
+                  box.push([oi, special_values[k], ("" + string_to_eval + ""), eval(string_to_eval)])
+                  console.log(counter + ": " + string_to_eval + ": ");//prefix hint
+                  console.log(eval(string_to_eval)); //run it
+                  console.log('');//generate a new line.
+              }catch(e){
+                  console.log(e);
+                  continue;
+              }
+          }// end of enum, special_values
+      }// end of enum, unary_operators
+    console.log(box)
+    return box
+  }
+  // the end of run unary;
+  return {values: special_values, operators: operators, unary: unary_operators, run_unary: run_unary, run_binary: run_binary};
 }()
 
+var app = angular.module('app', ['checklist-model']);
 app.filter('wrapstring', function(){
   return function(input){
      if (typeof input == 'string'){
@@ -108,6 +97,7 @@ app.filter('wrapstring', function(){
      return input;
   } 
 })
+
 app.controller('quirks', ['$scope', function($scope){
   $scope.filterStr = ''
   $scope.operators = Quirks.operators;
@@ -117,8 +107,8 @@ app.controller('quirks', ['$scope', function($scope){
   $scope.selected_operators = ['+', '&&'];
   $scope.selected_unary_operators    = ['typeof'];
   $scope.run = function(v, o, u, mode){
-    $scope.output_binary = binary(v, o, mode);
-    $scope.output_unary = unary(v, u);
+    $scope.output_binary = Quirks.run_binary(v, o, mode);
+    $scope.output_unary  = Quirks.run_unary(v, u);
   }
-  $scope.run($scope.selected_values,   $scope.selected_operators, $scope.selected_unary_operators, true);
+  $scope.run($scope.selected_values, $scope.selected_operators, $scope.selected_unary_operators, true);
 }])
